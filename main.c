a1163eb673efe74e36ef0a45056af6f055cd778c
zaidlaffta <bscmsc70@yahoo.com>
2020-02-01 17:43:44 -0800

    Add files via upload
    find the biggest number

--- main.c
+++ main.c
@@ -1,22 +1,19 @@
 //Notes here
-#include <stdio.h>
-#include <stdlib.h>
-
+int max(int x, int y);
 int main(){
-    char name[30];
-    int age;
-    char phon[33];
-    printf("Enter you name: ");
-    fgets(name, 30, stdin );
-    printf("Enter you age: ");
-    scanf("%d", &age);
-    getchar();
-    printf("Enter you phone number: ");
-    fgets(phon, 33, stdin);
-    printf("your name is: %s, and your age is: %d, and your phone number is %s", name, age, phon);
-    
-    
-    
+    int result;
+    result = max(44, 5);
+    printf("%d\n", result);
     return 0;
+    
 }
-
+int max(int x, int y){
+    
+    int maxn;
+    if (x > y)
+        maxn = x;
+    else
+        maxn = y;
+    
+    return maxn;
+}
